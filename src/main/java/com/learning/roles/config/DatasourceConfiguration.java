41b5be8ecda29ae30d6a892ec5af921e4d5b9339
Juan David Campillo Cepeda <juancawool@outlook.com>
2021-12-21 11:01:58 -0500

    Setting up databaese config

--- src/main/java/com/learning/roles/config/DatasourceConfiguration.java
+++ src/main/java/com/learning/roles/config/DatasourceConfiguration.java
@@ -1,16 +1,44 @@
 package com.learning.roles.config;
 
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class DatasourceConfiguration {
-    @Bean
-    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
-        return new NamedParameterJdbcTemplate(dataSource);
-    }
 
+  @Value("${roles_app.datasource.url}")
+  private String dataSourceUrl;
+
+  @Value("${roles_app.datasource.password}")
+  private String password;
+
+  @Value("${roles_app.datasource.username}")
+  private String user;
+
+  @Value("${roles_app.datasource.driver-class-name}")
+  private String driver;
+
+  @Bean
+  public DataSource dataSource() {
+    System.out.println("hola parce");
+    System.out.println(this.dataSourceUrl);
+    System.out.println(this.driver);
+    DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+    dataSourceBuilder.username(this.user);
+    dataSourceBuilder.password(this.password);
+    dataSourceBuilder.url(this.dataSourceUrl);
+    dataSourceBuilder.driverClassName(this.driver);
+    return dataSourceBuilder.build();
+  }
+
+  @Bean
+  public NamedParameterJdbcTemplate namedParameterJdbcTemplate(
+    DataSource dataSource
+  ) {
+    return new NamedParameterJdbcTemplate(dataSource);
+  }
 }
